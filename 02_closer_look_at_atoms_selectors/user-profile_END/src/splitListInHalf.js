2ada3be512ec3758372d9c7e4beddae95e496486
ohansemmanuel <ohansemmanuel@outlook.com>
2020-08-21 12:24:14 +0200

    delete user_profile_END

--- 02_closer_look_at_atoms_selectors/user-profile_END/src/splitListInHalf.js
+++
@@ -1,9 +0,0 @@
-export const splitListInHalf = (list) => {
-  if (!list.length) return [[], []];
-
-  const half = Math.ceil(list.length / 2);
-  const firstHalf = list.slice(0, half);
-  const secondHalf = list.slice(-half);
-
-  return [firstHalf, secondHalf];
-};
