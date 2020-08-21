e5a4a32822db662d810dffddbf1f657522685563
ohansemmanuel <ohansemmanuel@outlook.com>
2020-08-21 15:39:13 +0200

    cleanup

--- 02_closer_look_at_atoms_selectors/user-profile/src/App.js
+++ 02_closer_look_at_atoms_selectors/user-profile/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { atom, useRecoilState } from "recoil";
 import { fetchUserProfile } from "./api";
-import logo from "./logo.svg";
 import "./App.css";
 
 const userProfileState = atom({
